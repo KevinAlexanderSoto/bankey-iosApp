9feaeef7c9eb8015b7c3afef8c35af543070360b
kevin alexander soto <kevinalexandersoto999@gmail.com>
2023-12-16 14:00:35 -0500

    add login view and basic button

--- Bankey/LogIn/LogInView.swift
+++ Bankey/LogIn/LogInView.swift
@@ -8,7 +8,12 @@
 import Foundation
 import UIKit
 
-class LogInView: UIView{
+class LogInView: UIView {
+    
+    let userNameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let stackView = UIStackView()
+    let dividerView = UIView()
     
     override init (frame: CGRect){
         super.init(frame: frame)
@@ -19,10 +24,10 @@ class LogInView: UIView{
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height: 200)
-    }
+    //This intrinsicContentSize tells the view how big it gonna be
+    //override var intrinsicContentSize: CGSize {
+    //    return CGSize(width: 100, height: 100)
+    //}
 }
 
 extension LogInView {
@@ -31,8 +36,56 @@ extension LogInView {
         backgroundColor = .orange
         translatesAutoresizingMaskIntoConstraints = false
         
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        
+        userNameTextField.translatesAutoresizingMaskIntoConstraints = false
+        userNameTextField.placeholder = "Username"
+        userNameTextField.delegate = self
+        
+        dividerView.translatesAutoresizingMaskIntoConstraints = false
+        dividerView.backgroundColor = .secondarySystemFill
+        
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.delegate = self
+        
+        layer.cornerRadius = 8
+        clipsToBounds = true
+        backgroundColor = .lightGray
     }
     func layout( ){
+        stackView.addArrangedSubview(userNameTextField)
+        stackView.addArrangedSubview(dividerView)
+        stackView.addArrangedSubview(passwordTextField)
+        addSubview(stackView)
+        
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 1),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: stackView.bottomAnchor, multiplier: 1)
+        ])
+        dividerView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+}
+
+extension LogInView : UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        userNameTextField.endEditing(true)
+        passwordTextField.endEditing(true)
+        return true
+    }
+    
+    private func textFieldDidEndEditing(_ textField: UITextField) -> Bool {
+        return textField.text != "" ? true : false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
         
     }
 }
--- Bankey/LogIn/LoginViewController.swift
+++ Bankey/LogIn/LoginViewController.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
+// te view Controller handle events of our app
 class LoginViewController: UIViewController {
     
     let LoginView = LogInView()
-
+    let SignInButton = UIButton(type: .system)
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
@@ -25,19 +26,40 @@ extension LoginViewController {
     private func style(){
         
         LoginView.translatesAutoresizingMaskIntoConstraints = false
-        LoginView.backgroundColor = .orange
+        
+        SignInButton.translatesAutoresizingMaskIntoConstraints = false
+        SignInButton.configuration = .filled()
+        SignInButton.configuration?.imagePadding = 8
+        SignInButton.setTitle("Sign In", for: [])
+        SignInButton.addTarget(self, action: #selector(onSignInClick), for: .primaryActionTriggered)
     }
     
     
     private func layout( ){
         view.addSubview(LoginView)
+        view.addSubview(SignInButton)
         
+        //LoginView
         NSLayoutConstraint.activate([
             LoginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             LoginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: LoginView.trailingAnchor, multiplier: 1),
             
+            
         ])
+        //signInButtonView
+        NSLayoutConstraint.activate([
+            SignInButton.topAnchor.constraint(equalToSystemSpacingBelow: LoginView.bottomAnchor, multiplier: 1),
+            SignInButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: SignInButton.trailingAnchor, multiplier: 2),
+            
+        ])
+    }
+}
+
+extension LoginViewController {
+    @objc func onSignInClick (sender : UIButton){
+        
     }
 }
 
